c91dbaacf00511127fd3496ddb7ed228dee79f59
0xKryptonian <xerox336699@gmail.com>
2024-02-29 23:22:19 +0530

    creating basic test cases

---
+++ src/Faucet.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'expect';
+import { AccountUpdate, Mina, Permissions, TokenId, UInt64, PrivateKey, PublicKey } from 'o1js';
+import { getProfiler } from '../utils/profiler.js';
+import { TokenContract, addresses, createDex, keys, tokenIds } from './dex.js';
+
+let proofsEnabled = false;
+let Local = Mina.LocalBlockchain({
+  proofsEnabled,
+  enforceTransactionLimits: false,
+});
+Mina.setActiveInstance(Local);
+let [{ privateKey: feePayerKey, publicKey: feePayerAddress }] =
+  Local.testAccounts;
+let tx, balances, oldBalances;
+
+console.log('-------------------------------------------------');
+console.log('FEE PAYER\t', feePayerAddress.toBase58());
+console.log('TOKEN X ADDRESS\t', addresses.tokenX.toBase58());
+console.log('TOKEN Y ADDRESS\t', addresses.tokenY.toBase58());
+console.log('DEX ADDRESS\t', addresses.dex.toBase58());
+console.log('USER ADDRESS\t', addresses.user.toBase58());
+console.log('-------------------------------------------------');
+console.log('TOKEN X ID\t', TokenId.toBase58(tokenIds.X));
+console.log('TOKEN Y ID\t', TokenId.toBase58(tokenIds.Y));
+console.log('-------------------------------------------------');
+
+await TokenContract.analyzeMethods();
+if (proofsEnabled) {
+  console.log('compile (token)...');
+  await TokenContract.compile();
+}
+
+await main({ withVesting: false });
